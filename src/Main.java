e78073dbb5114d9b8ea2b9518955eeacf8152ec3
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2022-11-19 12:58:15 -0600

    floatanddouble

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float minimum value = "+ myMinFloatValue);
+        System.out.println("Float maximum value = "+ myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("double minimum value = "+ myMinDoubleValue);
+        System.out.println("double maximum value = "+ myMaxDoubleValue);
+
+        int myIntValue = 5;
+        float myFloatValue = 5f;
+        double myDoubleValue = 5d;
+        }
 }
